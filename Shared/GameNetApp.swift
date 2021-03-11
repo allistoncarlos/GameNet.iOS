4ab6783d5bbd9e307d60507044a7c4ec2911e9b8
Alliston <alliston@outlook.com>
2021-03-11 13:46:30 -0300

    Project Creation

---
+++ Shared/GameNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameNetApp.swift
+//  Shared
+//
+//  Created by Alliston Aleixo on 11/03/21.
+//
+
+import SwiftUI
+
+@main
+struct GameNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
